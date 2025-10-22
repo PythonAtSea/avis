590e194097e0eb5edae23c643c1f54e0d8d04155
PythonAtSea <HudsonGreen27@gmail.com>
2025-10-22 16:03:48 -0400

    feat: redo layout to three column layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,22 @@
 
 import { useEffect, useState } from "react";
 
+const scrollbarStyles = `
+  .custom-scrollbar::-webkit-scrollbar {
+    width: 8px;
+  }
+  .custom-scrollbar::-webkit-scrollbar-track {
+    background: rgba(0, 0, 0, 0.0);
+  }
+  .custom-scrollbar::-webkit-scrollbar-thumb {
+    background: rgba(120, 113, 108, 0.8);
+    border-radius: 4px;
+  }
+  .custom-scrollbar::-webkit-scrollbar-thumb:hover {
+    background: rgba(168, 162, 158, 0.8);
+  }
+`;
+
 type Bird = {
   birthDate: Date;
   isAdult: boolean;
@@ -46,6 +62,7 @@ export default function Home() {
   const [wormSpeedUpgradeCost, setWormSpeedUpgradeCost] = useState<number>(100);
   const [growUpgradeCost, setGrowUpgradeCost] = useState<number>(80);
   const [maxLifeUpgradeCost, setMaxLifeUpgradeCost] = useState<number>(150);
+  const [lastWormTime, setLastWormTime] = useState<number>(Date.now());
 
   const getOrdinal = (num: number): string => {
     const j = num % 10;
@@ -110,151 +127,198 @@ export default function Home() {
         }
         return prevBirds;
       });
+      setLastWormTime(Date.now());
     }, msPerWorm);
     return () => clearInterval(interval);
   }, [msPerWorm]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {}, 0);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="min-h-screen bg-stone-950 flex flex-col items-center justify-center p-4">
+    <div className="min-h-screen bg-stone-950 flex flex-col p-4">
+      <style>{scrollbarStyles}</style>
       <h1 className="text-5xl font-bold text-white mb-8 text-center">Avis</h1>
-      <div className="grid grid-cols-2 gap-4 mb-8 w-full max-w-md">
-        <div className="bg-stone-900 p-4 text-center shadow-offset">
-          <p className="text-stone-100 text-sm">
-            Worms:{" "}
-            {(
-              birds.filter((b) => b.isAdult).length *
-              (1000 / msPerWorm)
-            ).toFixed(2)}{" "}
-            per second
-          </p>
-          <p className="text-3xl font-bold text-white">{worms}</p>
+      <div className="flex flex-1 gap-4 min-h-0">
+        <div
+          className="w-80 overflow-y-auto custom-scrollbar"
+          style={{ height: "calc(100vh - 7rem)" }}
+        >
+          <h2 className="text-2xl font-bold text-white mb-4 sticky top-0 bg-stone-950">
+            Birds
+          </h2>
+          {birds
+            .sort((a, b) => b.birthDate.getTime() - a.birthDate.getTime())
+            .map((bird, index) => (
+              <div
+                key={index}
+                className="bg-stone-900 text-stone-100 p-4 mb-4 shadow-offset-lg"
+              >
+                <h2 className="text-xl font-bold mb-2">
+                  {bird.name} ({bird.isAdult ? "Adult" : "Chick"})
+                </h2>
+                <p>
+                  Born at{" "}
+                  <span className="text-stone-400 font-bold">
+                    {bird.birthDate.toLocaleString()}
+                  </span>
+                </p>
+                {(() => {
+                  const ageInMs =
+                    new Date().getTime() - bird.birthDate.getTime();
+                  const progress = bird.isAdult
+                    ? Math.min(
+                        ((ageInMs - growMs) / (maxLife - growMs)) * 100,
+                        100
+                      )
+                    : Math.min((ageInMs / growMs) * 100, 100);
+                  return (
+                    <div className="w-full bg-stone-700 h-2 mt-2">
+                      <div
+                        className="bg-stone-400 h-2"
+                        style={{ width: `${progress}%` }}
+                      ></div>
+                    </div>
+                  );
+                })()}
+              </div>
+            ))}
         </div>
-        <div className="bg-stone-900 p-4 text-center shadow-offset">
-          <p className="text-stone-100 text-sm">Woodcock stats</p>
-          <p className="">
-            <span>Adults: </span>
-            <span className="text-stone-100 font-bold">
-              {birds.filter((b) => b.isAdult).length}
-            </span>
-            <span> Chicks: </span>
-            <span className="text-stone-100 font-bold">
-              {birds.filter((b) => !b.isAdult).length}
-            </span>
-          </p>
-        </div>
-      </div>
-      <button
-        className="w-32 h-32 -full bg-stone-200 shadow-offset hover:bg-stone-400 transition-all mb-8 flex items-center justify-center"
-        onClick={hatchEgg}
-      >
-        <span className="text-2xl text-stone-800">Hatch a egg!</span>
-      </button>
-      <h1 className="text-2xl font-bold text-white mb-4 text-center">
-        Upgrades
-      </h1>
-      <div className="grid grid-cols-2 gap-4 mb-8 w-full max-w-md">
-        <div className="bg-stone-900 p-4 mb-4 shadow-offset">
-          <p className="text-stone-100 mb-2 font-semibold">
-            Worm Speed Upgrade
-          </p>
-          <p className="text-stone-300 text-sm mb-1">
-            Current: {(msPerWorm / 1000).toFixed(1)}s per worm per adult bird
-          </p>
-          <p className="text-stone-300 text-sm mb-2">
-            After upgrade: {(Math.floor(msPerWorm / 1.1) / 1000).toFixed(1)}s
-            per worm per adult bird
-          </p>
-          <button
-            onClick={() => {
-              if (worms >= wormSpeedUpgradeCost) {
-                setWorms(worms - wormSpeedUpgradeCost);
-                setMsPerWorm(Math.floor(msPerWorm / 1.1));
-                setWormSpeedUpgradeCost(Math.floor(wormSpeedUpgradeCost * 1.2));
-              }
-            }}
-            disabled={worms < wormSpeedUpgradeCost}
-            className="font-bold bg-stone-700 px-2 py-1 hover:bg-stone-600 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
-          >
-            Buy ({wormSpeedUpgradeCost} worms)
-          </button>
-        </div>
-        <div className="bg-stone-900 p-4 mb-4 shadow-offset">
-          <p className="text-stone-100 mb-2 font-semibold">Growth Upgrade</p>
-          <p className="text-stone-300 text-sm mb-1">
-            Current: {(growMs / 1000).toFixed(1)}s to grow
-          </p>
-          <p className="text-stone-300 text-sm mb-2">
-            After upgrade: {(Math.floor(growMs / 1.3) / 1000).toFixed(1)}s to
-            grow
-          </p>
-          <button
-            onClick={() => {
-              if (worms >= growUpgradeCost) {
-                setWorms(worms - growUpgradeCost);
-                setGrowMs(Math.floor(growMs / 1.1));
-                setGrowUpgradeCost(Math.floor(growUpgradeCost * 1.1));
-              }
-            }}
-            disabled={worms < growUpgradeCost}
-            className="font-bold bg-stone-700 px-2 py-1 hover:bg-stone-600 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
-          >
-            Buy ({growUpgradeCost} worms)
-          </button>
-        </div>
-        <div className="bg-stone-900 p-4 mb-4 shadow-offset">
-          <p className="text-stone-100 mb-2 font-semibold">Bird Life Upgrade</p>
-          <p className="text-stone-300 text-sm mb-1">
-            Current: {(maxLife / 1000).toFixed(0)}s max life
-          </p>
-          <p className="text-stone-300 text-sm mb-2">
-            After upgrade: {(Math.floor(maxLife * 1.2) / 1000).toFixed(0)}s max
-            life
-          </p>
-          <button
-            onClick={() => {
-              if (worms >= maxLifeUpgradeCost) {
-                setWorms(worms - maxLifeUpgradeCost);
-                setMaxLife(Math.floor(maxLife * 1.2));
-                setMaxLifeUpgradeCost(Math.floor(maxLifeUpgradeCost * 1.2));
-              }
-            }}
-            disabled={worms < maxLifeUpgradeCost}
-            className="font-bold bg-stone-700 px-2 py-1 hover:bg-stone-600 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
-          >
-            Buy ({maxLifeUpgradeCost} worms)
-          </button>
-        </div>
-      </div>
-      <div className="w-full max-w-md">
-        {birds.map((bird, index) => (
-          <div
-            key={index}
-            className="bg-stone-900 text-stone-100 p-4 mb-4 shadow-offset-lg"
-          >
-            <h2 className="text-xl font-bold mb-2">{bird.name}</h2>
-            <p>
-              Born at{" "}
-              <span className="text-stone-400 font-bold">
-                {bird.birthDate.toLocaleString()}
-              </span>
-            </p>
-            <p>Status: {bird.isAdult ? "Adult" : "Chick"}</p>
-            {(() => {
-              const ageInMs = new Date().getTime() - bird.birthDate.getTime();
-              const progress = bird.isAdult
-                ? Math.min(((ageInMs - growMs) / (maxLife - growMs)) * 100, 100)
-                : Math.min((ageInMs / growMs) * 100, 100);
-              return (
-                <div className="w-full bg-stone-700 h-2 mt-2">
-                  <div
-                    className="bg-stone-400 h-2"
-                    style={{ width: `${progress}%` }}
-                  ></div>
-                </div>
-              );
-            })()}
+
+        <div className="flex flex-col items-center justify-center flex-1 gap-8">
+          <div className="grid grid-cols-2 gap-4 w-full max-w-md">
+            <div className="bg-stone-900 p-4 shadow-offset">
+              <p className="text-stone-100 text-sm">
+                Worms:{" "}
+                {(
+                  birds.filter((b) => b.isAdult).length *
+                  (1000 / msPerWorm)
+                ).toFixed(2)}{" "}
+                per second
+              </p>
+              <p className="text-3xl font-bold text-white mb-2">{worms}</p>
+              <p className="text-stone-300 text-xs mb-2">Foraging Progress</p>
+              <div className="w-full bg-stone-700 h-2">
+                <div
+                  className="bg-amber-600 h-2"
+                  style={{
+                    width: `${
+                      birds.filter((b) => b.isAdult).length > 0
+                        ? Math.min(
+                            ((Date.now() - lastWormTime) / msPerWorm) * 100,
+                            100
+                          )
+                        : 0
+                    }%`,
+                  }}
+                ></div>
+              </div>
+            </div>
+            <div className="bg-stone-900 p-4 text-center shadow-offset">
+              <p className="text-stone-100 text-sm">Woodcock stats</p>
+              <p className="">
+                <span>Adults: </span>
+                <span className="text-stone-100 font-bold">
+                  {birds.filter((b) => b.isAdult).length}
+                </span>
+                <span> Chicks: </span>
+                <span className="text-stone-100 font-bold">
+                  {birds.filter((b) => !b.isAdult).length}
+                </span>
+              </p>
+            </div>
           </div>
-        ))}
+          <button
+            className="w-32 h-32 -full bg-stone-200 shadow-offset hover:bg-stone-400 flex items-center justify-center"
+            onClick={hatchEgg}
+          >
+            <span className="text-2xl text-stone-800">Hatch a egg!</span>
+          </button>
+        </div>
+
+        <div className="w-80 overflow-y-auto custom-scrollbar">
+          <h2 className="text-2xl font-bold text-white mb-4 sticky top-0 bg-stone-950">
+            Upgrades
+          </h2>
+          <div className="bg-stone-900 p-4 shadow-offset mb-4">
+            <p className="text-stone-100 mb-2 font-semibold">
+              Worm Speed Upgrade
+            </p>
+            <p className="text-stone-300 text-sm mb-1">
+              Current: {(msPerWorm / 1000).toFixed(1)}s per worm per adult bird
+            </p>
+            <p className="text-stone-300 text-sm mb-2">
+              After upgrade: {(Math.floor(msPerWorm / 1.1) / 1000).toFixed(1)}s
+              per worm per adult bird
+            </p>
+            <button
+              onClick={() => {
+                if (worms >= wormSpeedUpgradeCost) {
+                  setWorms(worms - wormSpeedUpgradeCost);
+                  setMsPerWorm(Math.floor(msPerWorm / 1.1));
+                  setWormSpeedUpgradeCost(
+                    Math.floor(wormSpeedUpgradeCost * 1.2)
+                  );
+                  setLastWormTime(Date.now());
+                }
+              }}
+              disabled={worms < wormSpeedUpgradeCost}
+              className="font-bold bg-stone-700 px-2 py-1 hover:bg-stone-600 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Buy ({wormSpeedUpgradeCost} worms)
+            </button>
+          </div>
+          <div className="bg-stone-900 p-4 shadow-offset mb-4">
+            <p className="text-stone-100 mb-2 font-semibold">Growth Upgrade</p>
+            <p className="text-stone-300 text-sm mb-1">
+              Current: {(growMs / 1000).toFixed(1)}s to grow
+            </p>
+            <p className="text-stone-300 text-sm mb-2">
+              After upgrade: {(Math.floor(growMs / 1.3) / 1000).toFixed(1)}s to
+              grow
+            </p>
+            <button
+              onClick={() => {
+                if (worms >= growUpgradeCost) {
+                  setWorms(worms - growUpgradeCost);
+                  setGrowMs(Math.floor(growMs / 1.3));
+                  setGrowUpgradeCost(Math.floor(growUpgradeCost * 1.1));
+                }
+              }}
+              disabled={worms < growUpgradeCost}
+              className="font-bold bg-stone-700 px-2 py-1 hover:bg-stone-600 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Buy ({growUpgradeCost} worms)
+            </button>
+          </div>
+          <div className="bg-stone-900 p-4 shadow-offset">
+            <p className="text-stone-100 mb-2 font-semibold">
+              Bird Life Upgrade
+            </p>
+            <p className="text-stone-300 text-sm mb-1">
+              Current: {(maxLife / 1000).toFixed(0)}s max life
+            </p>
+            <p className="text-stone-300 text-sm mb-2">
+              After upgrade: {(Math.floor(maxLife * 1.2) / 1000).toFixed(0)}s
+              max life
+            </p>
+            <button
+              onClick={() => {
+                if (worms >= maxLifeUpgradeCost) {
+                  setWorms(worms - maxLifeUpgradeCost);
+                  setMaxLife(Math.floor(maxLife * 1.2));
+                  setMaxLifeUpgradeCost(Math.floor(maxLifeUpgradeCost * 1.2));
+                }
+              }}
+              disabled={worms < maxLifeUpgradeCost}
+              className="font-bold bg-stone-700 px-2 py-1 hover:bg-stone-600 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Buy ({maxLifeUpgradeCost} worms)
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
